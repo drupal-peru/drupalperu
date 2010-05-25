9b48fddc6291c8fe3913f2ef79322b48904d28d9
Marco Villegas <marvil07@gmail.com>
2010-05-25 15:14:24 -0500

    detalle de estandares en plantilla de nodo

--- node.tpl.php
+++ node.tpl.php
@@ -1,19 +1,17 @@
-<div id="node-<?php print $node->nid ?>" class="node node-<?php print $node->type ?>">
+<div id="node-<?php print $node->nid; ?>" class="node node-<?php print $node->type; ?>">
 
-<?php if (!$page): ?>
-  <h2 class="teaser-title"><a href="<?php print $node_url ?>" title="<?php print $title ?>">
-    <?php print $title ?></a></h2>
-<?php endif; ?>
-
-	
+  <?php if (!$page): ?>
+    <h2 class="teaser-title"><a href="<?php print $node_url; ?>" title="<?php print $title; ?>">
+      <?php print $title ?></a></h2>
+  <?php endif; ?>
 
   <div class="content clear-block">
-    <?php print $picture ?>
-    <?php print $content ?>
+    <?php print $picture; ?>
+    <?php print $content; ?>
   </div>
 
-<?php if ($links): ?>
-  <div class="node-links"><?php print $links ?></div>
-<?php endif; ?>
+  <?php if ($links): ?>
+    <div class="node-links"><?php print $links; ?></div>
+  <?php endif; ?>
 
 </div>
